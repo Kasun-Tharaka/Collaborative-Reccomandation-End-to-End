5dad77a991be09d7c23132934b218de1aff5f260
Kasun Tharaka <k.tharakadharmapriya@gmail.com>
2025-07-03 22:33:39 +0530

    data ingestion completed

--- Book_Recommender/config/configuration.py
+++ Book_Recommender/config/configuration.py
@@ -7,35 +7,31 @@ from Book_Recommender.entity.config_entity import DataIngestionConfig
 from Book_Recommender.constant import *
 
 
-class AppConfigurations:
-    def __inint__(self, config_file_path: str = CONFIG_FILE_PATH):
+class AppConfiguration:
+    def __init__(self, config_file_path: str = CONFIG_FILE_PATH):
         try:
-            self.config_info = read_yaml_file(file_path=config_file_path)
-
+            self.configs_info = read_yaml_file(file_path=config_file_path)
         except Exception as e:
-            raise AppConfigurations(e, sys) from e
-        
+            raise AppException(e, sys) from e
 
-    # this function returns data ingestion configs.
+    
     def get_data_ingestion_config(self) -> DataIngestionConfig:
-
         try:
-            data_ingestion_config = self.config_info['data_ingestion_config']
-            artifacts_dir = self.config_info['artifacts_config']['artifacts_dir']
+            data_ingestion_config = self.configs_info['data_ingestion_config']
+            artifacts_dir = self.configs_info['artifacts_config']['artifacts_dir']
             dataset_dir = data_ingestion_config['dataset_dir']
 
-            ingested_data_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config
-                                             ['ingested_dir'])
+            ingested_data_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config['ingested_dir'])
             raw_data_dir = os.path.join(artifacts_dir, dataset_dir, data_ingestion_config['raw_data_dir'])
 
             response = DataIngestionConfig(
                 dataset_download_url = data_ingestion_config['dataset_download_url'],
                 raw_data_dir = raw_data_dir,
-                ingested_dir = ingested_data_dir
+                ingested_data = ingested_data_dir
             )
 
-            logging.info(f"data ingestion confic {response}")
+            logging.info(f"Data Ingestion Config: {response}")
             return response
-        
+
         except Exception as e:
             raise AppException(e, sys) from e
